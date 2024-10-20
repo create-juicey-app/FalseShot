e26ee9a1209a96a4ae7704c00408526870f3b7c0
Juicey <enzo.ignaes@gmail.com>
2024-10-21 00:51:05 +0200

    Add meta tags and title for Falseshot page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,18 @@ export default function Document() {
           type="font/ttf"
           crossOrigin="anonymous"
         />
+        <title>Falseshot</title>
+        <meta
+          property="description"
+          content="Nyko isn't worth saving anymore..."
+        />
+        <meta property="og:image" content="/banner.png"></meta>
+        <meta property="og:site_name" content="Falseshot"></meta>
+        <meta
+          property="og:description"
+          content="Nyko isn't worth saving anymore..."
+        />
+        <meta property="og:url" content="https://falseshot.vercel.app"></meta>
       </Head>
       <body>
         <Main />
